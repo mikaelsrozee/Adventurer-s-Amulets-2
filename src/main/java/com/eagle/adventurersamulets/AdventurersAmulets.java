577bd4286d31feeb60e3249cda4ce338c624fff0
Mikael Rozee <mikaelsrozee@gmail.com>
2018-10-29 19:14:58 +0000

    Further setting up

---
+++ src/main/java/com/eagle/adventurersamulets/AdventurersAmulets.java
@@ -0,0 +1,27 @@
+package com.eagle.adventurersamulets;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Dictionary.Core.MOD_ID, name = Dictionary.Core.MOD_NAME, version = Dictionary.Core.VERSION, dependencies = Dictionary.Core.DEPENDENCIES)
+public class AdventurersAmulets {
+
+  @EventHandler
+  public static void preInit(FMLPreInitializationEvent event) {
+
+  }
+
+  @EventHandler
+  public static void init(FMLInitializationEvent event) {
+
+  }
+
+  @EventHandler
+  public static void postInit(FMLPostInitializationEvent event) {
+
+  }
+
+}
---
+++ src/main/java/com/eagle/adventurersamulets/Dictionary.java
@@ -0,0 +1,12 @@
+package com.eagle.adventurersamulets;
+
+public class Dictionary {
+
+  public static class Core {
+    public static final String MOD_ID = "adventurersamulets";
+    public static final String MOD_NAME = "Adventurer's Amulets";
+    public static final String VERSION = "2.0.0-ALPHA1";
+    public static final String DEPENDENCIES = "required-after:baubles";
+  }
+
+}
