e76d9aae6076756a52dce970567517ada1717ef5
Mikael Rozee <mikaelsrozee@gmail.com>
2018-10-30 10:03:37 +0000

    Moved over to the JSON recipe format

--- src/main/java/com/eagle/adventurersamulets/init/ModRecipes.java
+++ src/main/java/com/eagle/adventurersamulets/init/ModRecipes.java
@@ -1,28 +1,17 @@
 package com.eagle.adventurersamulets.init;
 
-import com.eagle.adventurersamulets.Dictionary;
 import com.eagle.adventurersamulets.Dictionary.Core;
 import com.eagle.adventurersamulets.api.AdvAmAPI;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.IRecipe;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.oredict.ShapedOreRecipe;
 
 @EventBusSubscriber(modid = Core.MOD_ID)
 public class ModRecipes {
 
   public static void init() {
     initPestleRecipes();
-  }
-
-  @SubscribeEvent
-  public static void registerRecipes(RegistryEvent.Register<IRecipe> event) {
-    event.getRegistry().register(new ShapedOreRecipe(new ResourceLocation(Dictionary.Core.MOD_ID), new ItemStack(ModItems.pestle), " XX", " YX", "Y  ", 'X', "stone", 'Y', "cobblestone").setRegistryName(ModItems.pestle.getRegistryName()));
   }
 
   private static void initPestleRecipes() {
