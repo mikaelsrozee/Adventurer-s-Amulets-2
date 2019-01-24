fa3dcd7a7ca54f5a6207aa6ff45fcf87b579d7ac
Unknown <mikaelsrozee@gmail.com>
2019-01-24 11:40:54 +0000

    Bump version to 2.0.0-BETA-1 and release

--- src/main/java/com/eagle/adventurersamulets/Dictionary.java
+++ src/main/java/com/eagle/adventurersamulets/Dictionary.java
@@ -5,7 +5,7 @@ public class Dictionary {
   public static class Core {
     public static final String MOD_ID = "adventurersamulets";
     public static final String MOD_NAME = "Adventurer's Amulets";
-    public static final String VERSION = "2.0.0-ALPHA1";
+    public static final String VERSION = "2.0.0-BETA1";
     public static final String DEPENDENCIES = "required-after:baubles";
     public static final String COMMON_PROXY_CLASS = "com.eagle.adventurersamulets.proxy.CommonProxy";
     public static final String CLIENT_PROXY_CLASS = "com.eagle.adventurersamulets.proxy.ClientProxy";
